5b1341470fe7bd43f338a00ed364ee1371fa3269
Max DeBy <maxdeby@debygames.com>
2022-06-27 19:43:37 +0200

    Finished Binary Tree

--- BinaryTree/Program.cs
+++ BinaryTree/Program.cs
@@ -1,1 +1,148 @@
-﻿
+﻿using System.Security.Cryptography;
+using BinaryTree;
+using static System.Console;
+using static SharedUtilities.Extensions.ConsoleHelper;
+
+var rootNode = new Node(0, "Root");
+
+var tree = new Tree(rootNode);
+
+// Print initial menu
+Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+PrintLine("Statistics", ConsoleColor.Yellow, resetToDefault: true);
+
+Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+PrintLine("Add element", resetToDefault: true);
+
+Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+PrintLine("Find element", resetToDefault: true);
+
+Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+PrintLine("Remove element", resetToDefault: true);
+
+Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+PrintLine("Quit", resetToDefault: true);
+
+var optionIndex = 0;
+while (true)
+{
+    SetCursorPosition(0, 0);
+
+    var inputKey = ReadKey().Key;
+
+    if (inputKey == ConsoleKey.UpArrow) // Navigate menu
+    {
+        if (optionIndex == 0)
+            optionIndex = 4;
+        else
+            optionIndex--;
+    }
+    else if (inputKey == ConsoleKey.DownArrow) // Navigate menu
+    {
+        if (optionIndex == 4)
+            optionIndex = 0;
+        else
+            optionIndex++;
+    }
+    else if (inputKey == ConsoleKey.Enter) // Execute action
+    {
+        switch (optionIndex)
+        {
+            case 0:
+                {
+                    Clear();
+                    PrintLine($"Node count:\t{tree.NodeCount}");
+                    PrintLine($"Tree depth:\t{tree.DeepestLevel}");
+                    PrintEmptyLine();
+                    Print("Press key to continue...");
+                    WaitForInput();
+                    Clear();
+                }
+                break;
+
+            case 1:
+                {
+                    Clear();
+                    Print("Enter value to add to tree: ");
+                    var newValue = ReadLine();
+                    var key = BitConverter.ToInt16(RandomNumberGenerator.GetBytes(2));
+                    PrintEmptyLine();
+                    PrintLine($"Value \"{newValue}\" will be added to tree with key {key}");
+                    tree.Insert(key, newValue);
+                    PrintEmptyLine();
+                    Print("Press key to continue...");
+                    WaitForInput();
+                    Clear();
+                }
+                break;
+
+            case 2:
+                {
+                    Clear();
+                    var key = (short)ReadInt32("Enter the key of the node: ", cancelCallword: null, min: short.MinValue, max: short.MaxValue).Value;
+                    PrintEmptyLine();
+
+                    var node = tree.Find(key);
+                    if (node == null)
+                        PrintLine($"Could not find a node with key {key}.");
+                    else
+                    {
+                        PrintLine("Found node:");
+                        PrintLine($"- Key: {node.Key}");
+                        PrintLine($"- Value: {node.Value}");
+                        PrintLine($"- Comparisons: {tree.LastFindComparisonCount}");
+                    }
+
+                    PrintEmptyLine();
+                    Print("Press key to continue...");
+                    WaitForInput();
+                    Clear();
+                }
+                break;
+
+            case 3:
+                {
+                    Clear();
+                    Print("WARNING! ", ConsoleColor.Red, resetToDefault: true);
+                    PrintLine("Removing a node will lead to removal of its children as well, should it have more than one. Are you sure you want to continue? (Y/N)");
+
+                    if (ReadKey().Key != ConsoleKey.Y)
+                        PrintLine("Removal cancelled.");
+                    else
+                    {
+                        Clear();
+                        var key = (short)ReadInt32("Enter the key of the node: ", cancelCallword: null, min: short.MinValue, max: short.MaxValue).Value;
+                        tree.Remove(key);
+                        PrintLine("Node removed!");
+                    }
+
+                    PrintEmptyLine();
+                    Print("Press key to continue...");
+                    WaitForInput();
+                    Clear();
+                }
+                break;
+
+            case 4:
+                Environment.Exit(0);
+                break;
+        }
+    }
+
+    // Print menu, highlighting the current option
+
+    Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+    PrintLine("Statistics", optionIndex == 0 ? ConsoleColor.Yellow : ConsoleColor.Gray, resetToDefault: true);
+
+    Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+    PrintLine("Add element", optionIndex == 1 ? ConsoleColor.Yellow : ConsoleColor.Gray, resetToDefault: true);
+
+    Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+    PrintLine("Find element", optionIndex == 2 ? ConsoleColor.Yellow : ConsoleColor.Gray, resetToDefault: true);
+
+    Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+    PrintLine("Remove element", optionIndex == 3 ? ConsoleColor.Yellow : ConsoleColor.Gray, resetToDefault: true);
+
+    Print("> ", ConsoleColor.Yellow, resetToDefault: true);
+    PrintLine("Quit", optionIndex == 4 ? ConsoleColor.Yellow : ConsoleColor.Gray, resetToDefault: true);
+}
---
+++ BinaryTree/Tree.cs
@@ -0,0 +1,217 @@
+﻿namespace BinaryTree
+{
+    public class Tree
+    {
+        private uint _deepestLevel;
+        private uint _nodeCount;
+        private uint _lastFindComparisonCount;
+
+        /// <summary>
+        /// Creates a new tree with <see cref="root"/> as the root node.
+        /// </summary>
+        /// <param name="root">The root node</param>
+        public Tree(Node root)
+        {
+            Root = root;
+        }
+
+        /// <summary>
+        /// The root node.
+        /// </summary>
+        public Node Root { get; }
+        /// <summary>
+        /// The deepest level of the tree.
+        /// </summary>
+        public uint DeepestLevel => _deepestLevel;
+        /// <summary>
+        /// The amount of nodes in the tree.
+        /// </summary>
+        public uint NodeCount => _nodeCount;
+        /// <summary>
+        /// How many comparisons have been done in the last Find operation.
+        /// </summary>
+        public uint LastFindComparisonCount => _lastFindComparisonCount;
+
+        /// <summary>
+        /// Insert a new node with the key <see cref="key"/> and the value <see cref="value"/>.
+        /// </summary>
+        /// <param name="key">The key of the new node.</param>
+        /// <param name="value">The value of the new node.</param>
+        /// <returns>The inserted node.</returns>
+        public Node Insert(short key, string value)
+        {
+            var node = new Node(key, value);
+            InsertNode(node, Root);
+            _nodeCount++;
+
+            if (_deepestLevel < node.Level)
+                _deepestLevel = node.Level;
+
+            return node;
+        }
+
+        /// <summary>
+        /// Recursive method to insert a new node under a parent node.
+        /// </summary>
+        /// <param name="node">The node to be inserted</param>
+        /// <param name="parent">The current potential parent of the inserted node</param>
+        private void InsertNode(Node node, Node parent)
+        {
+            node.Level++;
+            if (node.Key > parent.Key)
+            {
+                if (parent.RightChild == null)
+                {
+                    parent.RightChild = node;
+                    node.Parent = parent;
+                }
+                else
+                    InsertNode(node, parent.RightChild);
+            }
+            else
+            {
+                if (parent.LeftChild == null)
+                {
+                    parent.LeftChild = node;
+                    node.Parent = parent;
+                }
+                else
+                    InsertNode(node, parent.LeftChild);
+            }
+        }
+
+        /// <summary>
+        /// Searches a node with the key <see cref="key"/> and returns it if it exists.
+        /// </summary>
+        /// <param name="key"></param>
+        /// <returns>A node with the key <see cref="key"/>, if it exists.</returns>
+        public Node Find(short key)
+        {
+            _lastFindComparisonCount = 0;
+            return FindNode(key, Root);
+        }
+
+        /// <summary>
+        /// Recursive method to find a node.
+        /// </summary>
+        /// <param name="key">The key to search for.</param>
+        /// <param name="searchNode">The current node that's being evaluated.</param>
+        /// <returns>A node with the key <see cref="key"/>, if it exists.</returns>
+        private Node FindNode(short key, Node searchNode)
+        {
+            _lastFindComparisonCount++;
+            if (searchNode.Key == key)
+                return searchNode;
+
+            _lastFindComparisonCount++;
+            if (key > searchNode.Key)
+            {
+                if (searchNode.RightChild == null)
+                    return null;
+                return FindNode(key, searchNode.RightChild);
+            }
+
+            if (searchNode.LeftChild == null)
+                return null;
+            return FindNode(key, searchNode.LeftChild);
+        }
+
+        /// <summary>
+        /// Removes a node from the tree based on its key, if it exists.
+        /// If the node has multiple children, they and the subsequent children will be removed as well.
+        /// If the node only has one child, it will be set as the new child of the removed node's parent.
+        /// </summary>
+        /// <param name="key"></param>
+        public void Remove(short key)
+        {
+            var node = Find(key);
+            if (node != null)
+            {
+                if (node == node.Parent.RightChild)
+                {
+                    node.Parent.RightChild = null;
+                    var loneChild = GetLoneChild(node);
+
+                    // If node has only one child, set that as the new child of the parent.
+                    if (loneChild != null)
+                    {
+                        node.Parent.RightChild = loneChild;
+                        ResetLevels(node.Parent);
+                    }
+                }
+                else
+                {
+                    node.Parent.LeftChild = null;
+                    var loneChild = GetLoneChild(node);
+
+                    // If node has only one child, set that as the new child of the parent.
+                    if (loneChild != null)
+                    {
+                        node.Parent.LeftChild = loneChild;
+                        ResetLevels(node.Parent);
+                    }
+                }
+            }
+
+            _nodeCount--;
+        }
+
+        /// <summary>
+        /// Reset the levels of a node and its children.
+        /// </summary>
+        /// <param name="node"></param>
+        private void ResetLevels(Node node)
+        {
+            if (node == null)
+                return;
+
+            node.Level = node.Parent.Level + 1;
+            ResetLevels(node.RightChild);
+            ResetLevels(node.LeftChild);
+        }
+
+        /// <summary>
+        /// Returns the child of a node if it has exactly one child. If there are none or two children, null will be returned.
+        /// </summary>
+        /// <param name="node">The node who's children are to be evaluated.</param>
+        /// <returns>The lone child of a node.</returns>
+        private Node GetLoneChild(Node node)
+        {
+            if (node.RightChild == null && node.LeftChild == null)
+                return null;
+
+            if (node.RightChild != null)
+            {
+                if (node.LeftChild != null)
+                    return null;
+                return node.RightChild;
+            }
+            
+            if (node.LeftChild != null)
+            {
+                if (node.RightChild != null)
+                    return null;
+                return node.LeftChild;
+            }
+
+            return null;
+        }
+    }
+
+    public class Node
+    {
+        public Node(short key, string value)
+        {
+            Key = key;
+            Value = value;
+        }
+
+        public short Key { get; }
+        public string Value { get; }
+
+        public Node Parent { get; set; }
+        public Node LeftChild { get; set; }
+        public Node RightChild { get; set; }
+        public uint Level { get; set; }
+    }
+}
